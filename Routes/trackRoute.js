725a3c4f345af8b11e68e1acd62449d4788698c3
Emmanuel worgu <emmanuelworgu@gmail.com>
2023-07-19 05:29:20 -0400

    testing

--- Routes/trackRoute.js
+++ Routes/trackRoute.js
@@ -33,9 +33,14 @@ route.post('/data', async (req, res) => {
 
 });
 
-route.get('/track-data', async (req, res) => {
+route.post('/track-data', async (req, res) => {
+    console.log('Server Requested...')
+    console.log(req.body);
+    console.log(req.headers);
     const data = await TrackModel.findOne({ trackCode: req.body.trackCode });
 
+    console.log('Data fetched. Ready to display');
+    console.log(data);
     return res.status(200).json({
         data,
     });
